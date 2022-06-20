c474084021c36ce500abd16683751561282d13ba
Divya Vummita <divyarao.vummetha@gmail.com>
2022-06-20 19:09:50 +0530

    tasthasbeendone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { ServicepageComponent } from './servicepage/servicepage.component';
+import { ContactpageComponent } from './contactpage/contactpage.component';
+import { BlogpageComponent } from './blogpage/blogpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutpageComponent,
+    ServicepageComponent,
+    ContactpageComponent,
+    BlogpageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servicepage/servicepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicepage',
+  templateUrl: './servicepage.component.html',
+  styleUrls: ['./servicepage.component.scss']
+})
+export class ServicepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
